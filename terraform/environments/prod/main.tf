53aec04b55f99ac8698e4e55bd01163390efa155
Rishit Kumar <140222791+rishikkkkkk@users.noreply.github.com>
2026-06-30 07:06:06 +0000

    Fix

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -17,7 +17,7 @@ terraform {
     key            = "prod/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
-    dynamodb_table = "devops-portfolio-tfstate-lock"
+    dynamodb_table = "devops-portfolio-tfstate-lock-prod"
   }
 }
 
@@ -59,7 +59,7 @@ module "iam" {
 }
 
 module "security_groups" {
-  source = "../../modules/security-groups"
+  source = "../../modules/security_groups"
 
   project_name = var.project_name
   environment  = var.environment
